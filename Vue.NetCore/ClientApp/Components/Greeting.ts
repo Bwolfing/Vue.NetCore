8a81d72c2fa6334367a4c43c348b4bebe357a6c9
Bwolfing <wolfington98@gmail.com>
2018-01-30 06:11:55 -0800

    Cleaning slate

--- Vue.NetCore/ClientApp/Components/Greeting.ts
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import Component from "vue-class-component";
-
-@Component({
-    name: "Greeting",
-})
-export default class Greeting extends Vue {
-    userName: string = "";
-}
